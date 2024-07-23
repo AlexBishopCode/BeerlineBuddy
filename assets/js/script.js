eff819eb37c5f0a8b0dfea7c930e5a232081fffd
Alexander Bishop <2kok8sho@students.codeinstitute.net>
2024-07-23 11:14:25 +0000

    add tickBox click event listener to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
-document.addEventListener('click', function(event) {
+const checkBoxes = document.getElementsByClassName('fa-circle');
+console.log(checkBoxes);
+function tickBox(event) {
     if(event.target.classList.contains('fa-circle')) {
-        event.classList.remove('fa-circle');
-        event.classList.add('fa-circle-check');
+        event.target.classList.remove('fa-circle');
+        event.target.classList.add('fa-circle-check');
     }
     console.log(event.target)
-})
+}
+
+for(const checkBox of checkBoxes) {
+
+checkBox.addEventListener('click', (event) => tickBox(event))
+}
